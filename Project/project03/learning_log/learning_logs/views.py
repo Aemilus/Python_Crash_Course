03f24540b6e95128833c5ed84311e6380ca5391a
Aemilus <35335174+Aemilus@users.noreply.github.com>
2022-02-27 14:10:31 +0200

    refactor

--- Project/project03/learning_log/learning_logs/views.py
+++ Project/project03/learning_log/learning_logs/views.py
@@ -5,6 +5,8 @@ from .forms import TopicForm, EntryForm
 
 
 # Create your views here.
+
+
 def index(request):
     """The home page for Learning Logs."""
     return render(request, 'learning_logs/index.html')
@@ -12,18 +14,18 @@ def index(request):
 
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.order_by('date_added')
-    context = {'topics': topics}
+    my_topics = Topic.objects.order_by('date_added')
+    context = {'topics': my_topics}
     return render(request, 'learning_logs/topics.html', context)
 
 
 def topic(request, topic_id):
     """Show a single topic identified by id and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    my_topic = Topic.objects.get(id=topic_id)
     # the minus sign in front of date_added sorts the results in reverse order,
     # which will display the most recent entries first
-    entries = topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries': entries}
+    entries = my_topic.entry_set.order_by('-date_added')
+    context = {'topic': my_topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
@@ -31,52 +33,52 @@ def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
         # no data submitted; create a blank form
-        form = TopicForm()
+        my_form = TopicForm()
     else:
         # POST data submitted; process the data
-        form = TopicForm(data=request.POST)
-        if form.is_valid():
-            form.save()
+        my_form = TopicForm(data=request.POST)
+        if my_form.is_valid():
+            my_form.save()
             return redirect('learning_logs:topics')
     # display a blank or invalid form
-    context = {'form': form}
+    context = {'form': my_form}
     return render(request, 'learning_logs/new_topic.html', context)
 
 
 def new_entry(request, topic_id):
     """Add a new entry on a specific topic."""
-    topic = Topic.objects.get(id=topic_id)
+    my_topic = Topic.objects.get(id=topic_id)
 
     if request.method != 'POST':
         # no data submitted; create a blank form
-        form = EntryForm()
+        my_form = EntryForm()
     else:
         # POST data submitted; process the data
-        form = EntryForm(data=request.POST)
-        if form.is_valid():
-            new_entry = form.save(commit=False)
-            new_entry.topic = topic
-            new_entry.save()
+        my_form = EntryForm(data=request.POST)
+        if my_form.is_valid():
+            my_new_entry = my_form.save(commit=False)
+            my_new_entry.topic = my_topic
+            my_new_entry.save()
             return redirect('learning_logs:topic', topic_id=topic_id)
     # display a blank or invalid form
-    context = {'topic': topic, 'form': form}
+    context = {'topic': my_topic, 'form': my_form}
     return render(request, 'learning_logs/new_entry.html', context)
 
 
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
-    entry = Entry.objects.get(id=entry_id)
-    topic = entry.topic
+    my_entry = Entry.objects.get(id=entry_id)
+    my_topic = my_entry.topic
 
     if request.method != 'POST':
         # initial request; pre-fill form with current entry in database
-        form = EntryForm(instance=entry)
+        my_form = EntryForm(instance=my_entry)
     else:
         # POST data submitted; process data
-        form = EntryForm(instance=entry, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('learning_logs:topic', topic_id=topic.id)
+        my_form = EntryForm(instance=my_entry, data=request.POST)
+        if my_form.is_valid():
+            my_form.save()
+            return redirect('learning_logs:topic', topic_id=my_topic.id)
 
-    context = {'entry': entry, 'topic': topic, 'form': form}
+    context = {'entry': my_entry, 'topic': my_topic, 'form': my_form}
     return render(request, 'learning_logs/edit_entry.html', context)

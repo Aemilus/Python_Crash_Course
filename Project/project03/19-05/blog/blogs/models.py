926430f5f6bcf9ca8f26637710d0cae9bb8a555e
Aemilus <35335174+Aemilus@users.noreply.github.com>
2022-02-27 22:00:22 +0200

    19-05

--- Project/project03/19-05/blog/blogs/models.py
+++ Project/project03/19-05/blog/blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -9,6 +10,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=80)
     text = models.TextField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
--- Project/project03/19-05/blog/blogs/views.py
+++ Project/project03/19-05/blog/blogs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import BlogPost
 from .forms import BlogPostForm
@@ -6,13 +8,20 @@ from .forms import BlogPostForm
 # Create your views here.
 
 
+def __check_blogpost_owner(_blogpost, _request):
+    if _blogpost.owner != _request.user:
+        raise Http404
+
+
 def index(request):
     """The home page for blogs."""
+    # noinspection PyUnresolvedReferences
     my_posts = BlogPost.objects.order_by('date_added')
     context = {'blog_posts': my_posts}
     return render(request, 'blogs/index.html', context)
 
 
+@login_required
 def new_post(request):
     """Add a new blog post."""
     if request.method != 'POST':
@@ -21,16 +30,21 @@ def new_post(request):
     else:
         form = BlogPostForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            my_new_post = form.save(commit=False)
+            my_new_post.owner = request.user
+            my_new_post.save()
             return redirect('blogs:index')
 
     context = {'form': form}
     return render(request, 'blogs/new_post.html', context)
 
 
+@login_required
 def edit_post(request, post_id):
     """Edit an existing blog post."""
+    # noinspection PyUnresolvedReferences
     my_post = BlogPost.objects.get(id=post_id)
+    __check_blogpost_owner(my_post, request)
     if request.method != 'POST':
         # pre-fill form with my_post
         my_form = BlogPostForm(instance=my_post)
